7501b5bba820dfd3296d8a684381d464ad6f088f
Jacek Jarosz <jacekjarosz55@gmail.com>
2025-11-22 14:00:26 +0100

    Add Bag logic, materials, finish milestone perhaps

--- Assets/Scripts/Glove.cs
+++ Assets/Scripts/Glove.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Glove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Vector3 currentVelocity = Vector2.zero;
+    private Vector3 lastPosition;
+    public Vector3 Velocity { get => currentVelocity; }
+
     void Start()
     {
+        lastPosition = transform.position;
         
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentVelocity = (lastPosition - transform.position) / Time.deltaTime;
+        lastPosition = transform.position;
     }
 }
--- Assets/Scripts/PunchBag.cs
+++ Assets/Scripts/PunchBag.cs
@@ -1,17 +1,25 @@
+using TMPro;
 using UnityEngine;
 
 public class PunchBag : MonoBehaviour
 {
+
+    public TMP_Text displayOutput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
-    void OnCollisionEnter(Collision col)
+
+
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"Hit!, {col.relativeVelocity * 1000}");
+        Glove glove = other.GetComponent<Glove>();
+        displayOutput.text = $"Score: {Mathf.Floor(glove.Velocity.magnitude * 500)}";
     }
+
 
     // Update is called once per frame
     void Update()
