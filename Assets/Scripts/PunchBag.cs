8790fd362cabac76f64e0d405fa89e2f228463a0
Jacek Jarosz <jacekjarosz55@gmail.com>
2026-01-21 17:11:08 +0100

    new hit animation

--- Assets/Scripts/PunchBag.cs
+++ Assets/Scripts/PunchBag.cs
@@ -13,6 +13,7 @@ public class PunchBag : MonoBehaviour
 
     public TMP_Text displayOutput;
     public AnimationCurve scoreFunction;
+    public AnimationCurve dampeningFunction;
     public float maxForce = 16.0f;
     private float score;
     private float writtenScore = 0;
@@ -20,13 +21,37 @@ public class PunchBag : MonoBehaviour
 
     private bool active = true;
     public bool IsActive => active;
+    public GameObject particlesPrefab;
+
+    private HingeJoint hinge;
+    private JointSpring jointSpring;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        hinge = GetComponent<HingeJoint>();
+        jointSpring = new JointSpring();
+        hinge.useSpring = true;
+        hinge.spring = jointSpring;
+        jointSpring.damper = dampeningFunction.Evaluate(0);
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        Collider other = collision.collider;
+        if (!active) return;
+        if (other.TryGetComponent(out Glove glove)) {
+            foreach (var contact in collision.contacts)
+            {
+                Instantiate(particlesPrefab, contact.point, Quaternion.identity);
+            }
+
+            score = Mathf.Floor(scoreFunction.Evaluate(glove.Velocity.magnitude/maxForce)*999);
+            Debug.Log($"Velocity: {glove.Velocity.magnitude}, Score: {score}, Rel. Velocity: {collision.relativeVelocity}");
+            active = false;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -54,6 +79,7 @@ public class PunchBag : MonoBehaviour
 
     private IEnumerator HandleReset()
     {
+        jointSpring.damper = dampeningFunction.Evaluate(0);
         writtenScore = 0;
         score = 0;
         yield return new WaitForSeconds(2);
@@ -67,7 +93,9 @@ public class PunchBag : MonoBehaviour
     {
         if (Mathf.Abs(writtenScore - score) > 0.1) {
             writtenScore = Mathf.Lerp(writtenScore, score, Time.deltaTime);
-            displayOutput.text = $"{FormatScore(writtenScore)}";
+            displayOutput.text = $"{FormatScore(writtenScore+1)}";
+            jointSpring.damper = dampeningFunction.Evaluate(writtenScore / score);
+            hinge.spring = jointSpring;
         }
         else if (score != 0)
         {
